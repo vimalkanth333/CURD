21b286ad70cecf091bca8401ddf82595b63e4228
vimalkanth <131942639+vimalkanth333@users.noreply.github.com>
2024-01-31 10:31:34 +0530

    Users.jsx

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -18,7 +18,7 @@ function Users () {
         .then(res => {console.log(res)
             })
         .catch(err => console.log(err))
-    }
+    },[];
     return (
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className='w-50 bg-white rounded p-3'>
