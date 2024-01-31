30f5c504ccfa49797b0741d5f7105aa77db3bc3c
vimalkanth <131942639+vimalkanth333@users.noreply.github.com>
2024-01-31 10:32:55 +0530

    Users.jsx

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -16,9 +16,9 @@ function Users () {
     const handleDelete =(id) =>{
         axios.delete(`${baseURL}/deleteUser/`+id)
         .then(res => {console.log(res)
-            })
+           [] })
         .catch(err => console.log(err))
-    },[];
+    }
     return (
         <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className='w-50 bg-white rounded p-3'>
