e86985785b2ad33d96f62589f0861e15269d5a19
vimalkanth <131942639+vimalkanth333@users.noreply.github.com>
2024-01-31 10:16:02 +0530

    Users.jsx

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -20,7 +20,7 @@ function Users () {
         .catch(err => console.log(err))
     }
     return (
-        <div className="d-flex mw-100 bg-primary justify-content-center align-items-center">
+        <div className="d-flex vh-100 bg-primary justify-content-center align-items-center">
             <div className='w-50 bg-white rounded p-3'>
                 <Link to="/create" className="btn btn-success">Add +</Link>
                 <table className='table'>
